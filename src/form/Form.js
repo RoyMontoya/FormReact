540295c31c5f4da6891bf9a8df4492254bf467e8
Roy Montoya <roymontoya89@gmail.com>
2017-11-26 18:28:36 -0600

    init commit

---
+++ src/form/Form.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import FormRow from './FormRow'
+
+class Form extends Component{
+
+  render(){
+    return(
+      <div>
+        <h2>
+          Registro
+        </h2>
+
+      </div>
+    );
+  }
+}
+
+export default Form;
---
+++ src/form/FormRow.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class FormRow extends Component{
+
+  render(){
+    return(
+      <div>
+        <label>Label</label>
+        <Input/>
+      </div>
+    );
+  }
+}
+
+export default FormRow;

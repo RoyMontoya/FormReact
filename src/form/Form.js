d50070723334e2aed8c35f4dc5f47ddc900d6f41
Roy Montoya <roymontoya89@gmail.com>
2017-11-26 18:51:21 -0600

    created components and made a form in app

--- src/form/Form.js
+++ src/form/Form.js
@@ -7,10 +7,12 @@ class Form extends Component{
   render(){
     return(
       <div>
-        <h2>
-          Registro
-        </h2>
-
+        <h2>Registro</h2>
+        <FormRow inputType="text" labelText ="Nombre"/>
+        <FormRow inputType="text" labelText ="Apellido"/>
+        <FormRow inputType="email" labelText ="Correo Electronico"/>
+        <FormRow inputType="password" labelText ="Password"/>
+        <FormRow inputType="password" labelText ="Confirmar Password"/>
       </div>
     );
   }
--- src/form/FormRow.js
+++ src/form/FormRow.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types'
+
 class FormRow extends Component{
+  constructor(props){
+    super(props);
+
+
+      this.onChange = this.onChange.bind(this);
+      this.getValue = this.getValue.bind(this);
+      this.state = {
+        inputValue: ''
+      }
+  }
+
+  onChange(e){
+    this.setState({inputValue:e.target.value});
+  }
+
+  getValue(){
+    return this.state.inputValue;
+  }
 
   render(){
     return(
       <div>
-        <label>Label</label>
-        <Input/>
+        <label>{this.props.labelText}</label>
+        <input type = {this.props.inputType} value = {this.state.inputValue}
+        onChange = {this.onChange}/>
       </div>
     );
   }
+}
+
+FormRow.propTypes = {
+  inputType: PropTypes.string,
+  labelText: PropTypes.string
 }
 
 export default FormRow;
